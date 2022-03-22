c3cf43f7adeea199d15ad9761ab7759f2d76c04a
Dan Lien <dan@eduphoria.net>
2022-03-22 14:26:04 -0500

    Switch to latest version supported by Consul: v1.20.2

--- extract-envoy.ps1
+++ extract-envoy.ps1
@@ -4,7 +4,7 @@ param(
 )
 
 $dockerUrl = "https://download.docker.com/win/static/stable/x86_64/docker-20.10.13.zip"
-$dockerImage = "envoyproxy/envoy-windows:v1.21-latest"
+$dockerImage = "envoyproxy/envoy-windows:v1.20.2"
 
 # Check for administrator
 function Test-Admin {
